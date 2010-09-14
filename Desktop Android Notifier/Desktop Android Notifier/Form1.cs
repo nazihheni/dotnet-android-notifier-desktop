c9c7e19d36913bbc3b6fad4c9097c287171fcdf2
jnosek <jnosek@66f78004-169d-58d2-08ac-b90340367355>
2010-09-14 17:41:55 +0000

    initial test, a sample notification pops up and the ping test message can be captured

---
+++ Desktop Android Notifier/Desktop Android Notifier/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using Hardcodet.Wpf.TaskbarNotification;
+using Desktop_Android_Notifier.Properties;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using Desktop_Android_Notifier.Service;
+
+namespace Desktop_Android_Notifier
+{
+    public partial class Form1 : Form
+    {
+        private 
+
+        TaskbarIcon icon;
+        NotificationReceiver server = new NotificationReceiver();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            icon.ShowBalloonTip("TXT Message From John Doe", "Hello world", Resources.android_sms);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server.MessageEvent += new Action<string>(server_MessageEvent);
+            server.Start();
+
+            icon = new TaskbarIcon();
+            icon.Icon = TaskbarIcon.DefaultIcon;
+            icon.TrayBalloonTipClicked += new System.Windows.RoutedEventHandler(icon_TrayBalloonTipClicked);
+        }
+
+        void server_MessageEvent(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        void icon_TrayBalloonTipClicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show("Here are your messages!");
+        }
+    }
+}
---
+++ Desktop Android Notifier/Desktop Android Notifier/Service/NotificationReceiver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace Desktop_Android_Notifier.Service
+{
+    class NotificationReceiver
+    {
+        static readonly Guid BluetoothNotificationSerivce = Guid.Parse("7674047E-6E47-4BF0-831F-209E3F9DD23F");
+
+        BluetoothListener listener;
+
+        public event Action<string> MessageEvent;
+
+        public NotificationReceiver()
+        {
+            listener = new BluetoothListener(BluetoothNotificationSerivce);
+            listener.ServiceName = "AndroidNotifierService";
+        }
+
+        public void Start()
+        {
+            listener.Start();
+            listener.BeginAcceptBluetoothClient(new AsyncCallback(AcceptClient), null);
+        }
+
+        public void AcceptClient(IAsyncResult result)
+        {
+            byte[] buffer = new byte[1024];
+            StringBuilder message = new StringBuilder();
+
+            try
+            {
+                using (var connection = listener.EndAcceptBluetoothClient(result))
+                {
+                    using (var stream = connection.GetStream())
+                    {
+                        int bytesRead = 0;
+                        while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            message.Append(Encoding.ASCII.GetChars(buffer, 0, bytesRead));
+                        }
+
+                        stream.Close();
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                message.Append(ex.Message);
+            }
+            finally
+            {
+                if (MessageEvent != null)
+                    MessageEvent(message.ToString());
+
+                listener.BeginAcceptBluetoothClient(new AsyncCallback(AcceptClient), null);
+            }
+        }
+    }
+}
